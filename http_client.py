aad258da2d88dbb727127cfdbd5dfa219f85e9db
Claw <bossclawest@outlook.com>
2026-01-30 18:42:52 -0500

    feat : add ssl'

---
+++ http_client.py
@@ -0,0 +1,50 @@
+# NAME Darryl LeCraw
+# NUM n01712877
+
+from socket import *
+# NEW - SSL IMPORT
+import ssl
+
+# SETUP TARG DEETS
+server_name = 'gaia.cs.umass.edu'
+
+# NEW - SSL PORT
+server_port = 443
+
+
+# 1. Create a TCP socket (IPv4)
+# Hint: Use AF_INET for IPv4 and SOCK_STREAM for TCP
+client_socket = socket(AF_INET, SOCK_STREAM)
+
+
+# NEW - CREATE ENCRYPTION CONTEXT & WRAP SOCKET
+context = ssl.create_default_context()
+client_socket = context.wrap_socket(client_socket, server_hostname=server_name)
+
+
+# 2. Connect to the server
+client_socket.connect((server_name, server_port))
+
+
+# 3. Prepare the HTTP request
+# Critical: HTTP requires carriage return & new line (\r\n) at the end of headers
+# The double \r\n\r\n indicates the end of the header section.
+request = "GET /kurose_ross/interactive/index.php HTTP/1.1\r\nHost: gaia.cs.umass.edu\r\n\r\n"
+
+
+# 4. Send the request
+# We must encode the string into bytes before sending
+client_socket.send(request.encode())
+
+# 5. Receive the response
+# We read up to 4096 bytes
+response = client_socket.recv(4096)
+
+
+# 6. Decode and print the result
+decoded_response = response.decode()
+print(decoded_response)
+
+
+# NEW - CLOSE
+client_socket.close()
